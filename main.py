4f9e27af50f2ad5c149d5c35fea028b15f81f6e6
swinkdtw <david.swinkels@movares.nl>
2019-03-29 08:02:28 +0100

    Fix routes download. Start working on bounding boxes shuffler

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ from shapely.geometry import shape
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import requests
+import json
+from pprint import pprint
+
 
 ## Constant variables
+input_min_lat = 50.7
+input_min_lon = 5.6
+input_max_lat = 51.0
+input_max_lon = 6.2
+
+
+def create_bounding_boxes(min_lat, min_lon, max_lat, max_lon, rows=6, columns=12):
+    
+
+
 route_search_url = "https://api.routeyou.com/2.0/json/Route/k-9aec2fc1705896b901c3ea17d6223f0a/mapSearch"
-route_url = "https://download.routeyou.com/k-9aec2fc1705896b901c3ea17d6223f0a/route/5653357.gpx?language=nl"
+route_url = "https://download.routeyou.com/k-9aec2fc1705896b901c3ea17d6223f0a/route/5907767.gpx?language=nl"
 route_search_headers = {"Accept": "*/*",
                         "Accept-Encoding": "gzip, deflate, br",
                         "Accept-Language": "nl,en-US;q=0.7,en;q=0.3",
@@ -20,27 +33,6 @@ route_search_headers = {"Accept": "*/*",
                         "Referer": "https://www.routeyou.com/route/search/2/walking-route-search",
                         "TE": "Trailers",
                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"}
-
-route_search_params = {"jsonrpc": "2.0",
-                       "id": "3",
-                       "method": "searchAdvanced",
-                       "params": [{"bounds": {"min": {"lat": 50.400370273479346,
-                                                      "lon": 4.974064280094581},
-                                              "max": {"lat": 52.118895633404020,
-                                                      "lon": 6.819767405094581}},
-                                   "type.id": 2,
-                                   "score.min": 0.5,
-                                   "bounds.comparator": "geometry"},
-                                  "null",
-                                  20,
-                                  0,
-                                  {"clusters": "false",
-                                   "addLanguage":"en",
-                                   "media": "true",
-                                   "description": "true"}]}
-route_search_data = {"mimeType": "text/plain;charset=UTF-8",
-                     "params": [],
-                     "text": "{\"jsonrpc\":\"2.0\",\"id\":\"4942\",\"method\":\"searchAdvanced\",\"params\":[{\"bounds\":{\"min\":{\"lat\":50.400370273479346,\"lon\":4.974064280094581},\"max\":{\"lat\":52.11889563340402,\"lon\":6.819767405094581}},\"type.id\":2,\"score.min\":0.5,\"bounds.comparator\":\"geometry\"},null,20,0,{\"clusters\":false,\"addLanguage\":\"en\",\"media\":true,\"description\":true}]}"}
 default_headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                    "Accept-Encoding": "gzip, deflate, br",
                    "Accept-Language": "nl,en-US;q=0.7,en;q=0.3",
@@ -55,14 +47,25 @@ default_headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=
                    "TE": "Trailers",
                    "Upgrade-Insecure-Requests": "1",
                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"}
-filepath = "C:/Users/David/Downloads/Wandelroutes/5653357.gpx"
+filepath = "C:/Users/swinkdtw/Downloads/Wandelroutes/5653357.gpx"
 
-## Setup script
+# Setup script
+# route_search_data = '{"jsonrpc":"2.0","id":"3","method":"searchAdvanced","params":[{"bounds":{"min":{"lat":%s,"lon":%s},"max":{"lat":%s,"lon":%s}},"type.id":2,"score.min":0.5,"bounds.comparator":"geometry"},null,20,0,{"clusters":false,"addLanguage":"en","media":false,"description":false}]}' % (min_lat, min_lon, max_lat, max_lon)
+# print(route_search_data)
+# response = requests.post(url=route_search_url, headers=route_search_headers,
+#                          data=route_search_data)
+# print(response)
+#
+# # with open("./data/routes.txt", "wb") as file:
+# #     file.write(response.content)
+# #
+# data = json.loads(response.content)
+# pprint(data)
+# print("Routes count", len(data['result']['routes']))
+# print("Total routes", data['result']['total'])
+# print("Taken routes", data['result']['took'])
 
-response = requests.post(url=route_search_url, headers=route_search_headers,
-                         data=route_search_data)
-print(response)
-print(response.content)
+
 
 # rows_list = []
 # input_rows = [1, 2, 3]
@@ -90,6 +93,3 @@ print(response.content)
 # plt.show()
 # print(routes_gdf.geometry)
 # print(routes_gdf.crs)
-
-
-
