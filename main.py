9ceaee0b96507149086aef7081790a09ab6b3653
swinkdtw <david.swinkels@movares.nl>
2019-03-31 23:05:06 +0200

    Fix routes download. Iterate over downloaded routes. Save gpx files. Iterate over stored gpx files to add them to a shapefile. Product finished

--- main.py
+++ main.py
@@ -3,24 +3,18 @@ import fiona
 from shapely.geometry import shape
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from pprint import pprint
 import requests
 import json
-from pprint import pprint
+import time
+import os
 
-
-## Constant variables
-input_min_lat = 50.7
-input_min_lon = 5.6
-input_max_lat = 51.0
-input_max_lon = 6.2
-
-
-def create_bounding_boxes(min_lat, min_lon, max_lat, max_lon, rows=6, columns=12):
-    
-
-
+# Constant variables
+input_min_lat = 50.751797561
+input_min_lon = 5.726110232
+input_max_lat = 50.938216069
+input_max_lon = 6.121604582
 route_search_url = "https://api.routeyou.com/2.0/json/Route/k-9aec2fc1705896b901c3ea17d6223f0a/mapSearch"
-route_url = "https://download.routeyou.com/k-9aec2fc1705896b901c3ea17d6223f0a/route/5907767.gpx?language=nl"
 route_search_headers = {"Accept": "*/*",
                         "Accept-Encoding": "gzip, deflate, br",
                         "Accept-Language": "nl,en-US;q=0.7,en;q=0.3",
@@ -47,49 +41,47 @@ default_headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=
                    "TE": "Trailers",
                    "Upgrade-Insecure-Requests": "1",
                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"}
-filepath = "C:/Users/swinkdtw/Downloads/Wandelroutes/5653357.gpx"
-
-# Setup script
-# route_search_data = '{"jsonrpc":"2.0","id":"3","method":"searchAdvanced","params":[{"bounds":{"min":{"lat":%s,"lon":%s},"max":{"lat":%s,"lon":%s}},"type.id":2,"score.min":0.5,"bounds.comparator":"geometry"},null,20,0,{"clusters":false,"addLanguage":"en","media":false,"description":false}]}' % (min_lat, min_lon, max_lat, max_lon)
-# print(route_search_data)
-# response = requests.post(url=route_search_url, headers=route_search_headers,
-#                          data=route_search_data)
-# print(response)
-#
-# # with open("./data/routes.txt", "wb") as file:
-# #     file.write(response.content)
-# #
-# data = json.loads(response.content)
-# pprint(data)
-# print("Routes count", len(data['result']['routes']))
-# print("Total routes", data['result']['total'])
-# print("Taken routes", data['result']['took'])
 
 
+# # Setup script
+# bounding_boxes_list = create_bounding_boxes(input_min_lat, input_min_lon, input_max_lat, input_max_lon,
+#                                             nr_of_rows=12, nr_of_columns=12)
+# for index, bounding_box in enumerate(bounding_boxes_list):
+#     route_search_data = '{"jsonrpc":"2.0","id":"3","method":"searchAdvanced","params":' \
+#                         '[{"bounds":{"min":{"lat":%s,"lon":%s},"max":{"lat":%s,"lon":%s}},' \
+#                         '"type.id":2,"score.min":0.5,"bounds.comparator":"geometry"},null,100,0,' \
+#                         '{"clusters":false,"addLanguage":"en","media":false,"description":false}]}' \
+#                         % (bounding_box['min_lat'], bounding_box['min_lon'], bounding_box['max_lat'], bounding_box['max_lon'])
+#     response = requests.post(url=route_search_url, headers=route_search_headers,
+#                              data=route_search_data)
+#     with open("D:/Wandelroutes/Text/routes_{}.txt".format(index), "wb") as file:
+#         file.write(response.content)
+#     data = json.loads(response.content)
+#     print("Index / routes count / total routes: ", index, "/", len(data['result']['routes']), "/", data['result']['total'])
+#
+#     for route in data['result']['routes']:
+#         time.sleep(0.5)
+#         route_url = "https://download.routeyou.com/k-9aec2fc1705896b901c3ea17d6223f0a/route/{}.gpx?language=nl".format(route['id'])
+#         filepath = "D:/Wandelroutes/GPX/{}.gpx".format(route['id'])
+#         download_to_file(route_url, default_headers, filepath)
 
-# rows_list = []
-# input_rows = [1, 2, 3]
-# download_to_file(route_url, default_headers, filepath)
-# for row in input_rows:
-#
-#     layer = fiona.open(filepath, layer='tracks')
-#     geom = layer[0]
-#     route_name = geom['properties']['name']
-#     route_geodata = {'type': 'MultiLineString',
-#                      'coordinates': geom['geometry']['coordinates']}
-#     print(route_geodata)
-#     route_geometry = shape(route_geodata)
-#     route_dict = {'name': route_name,
-#                   'geometry': route_geometry}
-#     rows_list.append(route_dict)
-#
-#
-# print(rows_list)
-# routes_gdf = gpd.GeoDataFrame(rows_list)
-# routes_gdf.crs = {'init': 'epsg:4326', 'no_defs': True}
-# print(routes_gdf.crs)
-# # routes_gdf.to_crs({'init': 'epsg:28992'})
-# routes_gdf.plot()
-# plt.show()
-# print(routes_gdf.geometry)
-# print(routes_gdf.crs)
+dir_filepath = "D:/Wandelroutes/GPX"
+filenames = os.listdir(dir_filepath)
+rows_list = []
+for filename in filenames:
+    layer = fiona.open(os.path.join(dir_filepath, filename), layer='tracks')
+    geom = layer[0]
+    route_name = geom['properties']['name']
+    route_geodata = {'type': 'MultiLineString',
+                     'coordinates': geom['geometry']['coordinates']}
+    route_geometry = shape(route_geodata)
+    route_id = os.path.splitext(os.path.basename(filename))[0]
+    route_dict = {'id': str(route_id),
+                  'name': route_name,
+                  'url': "https://www.routeyou.com/nl-nl/route/view/" + str(route_id),
+                  'geometry': route_geometry}
+    rows_list.append(route_dict)
+
+routes_gdf = gpd.GeoDataFrame(rows_list)
+routes_gdf.crs = {'init': 'epsg:4326', 'no_defs': True}
+routes_gdf.to_file("D:/Wandelroutes/walking_routes.shp")
--- scripts/downloader.py
+++ scripts/downloader.py
@@ -20,3 +20,23 @@ def download_content(url, headers):
 def read_gpx_to_geodataframe(input_filepath):
     input_gdf = gpd.read_file(input_filepath)
     return input_gdf
+
+
+def create_bounding_boxes(min_lat, min_lon, max_lat, max_lon, nr_of_rows=6, nr_of_columns=12):
+    bounding_boxes_list = []
+    delta_lat = max_lat - min_lat
+    delta_lon = max_lon - min_lon
+    dist_lat = delta_lat / nr_of_rows
+    dist_lon = delta_lon / nr_of_columns
+    for row_nr in range(nr_of_rows):
+        for column_nr in range(nr_of_columns):
+            min_lat_window = min_lat + (dist_lat * row_nr)
+            min_lon_window = min_lon + (dist_lon * column_nr)
+            max_lat_window = min_lat + (dist_lat * (row_nr + 1))
+            max_lon_window = min_lon + (dist_lon * (column_nr + 1))
+            bounding_box_dict = {"min_lat": min_lat_window,
+                                 "min_lon": min_lon_window,
+                                 "max_lat": max_lat_window,
+                                 "max_lon": max_lon_window}
+            bounding_boxes_list.append(bounding_box_dict)
+    return bounding_boxes_list
